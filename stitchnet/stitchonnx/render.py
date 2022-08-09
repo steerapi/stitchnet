9f0bfdf8099faa17c41fe22b3370e1355017b150
Surat Teerapittayanon <steerapi@gmail.com>
2022-08-09 22:08:26 +0700

    add graph rendering

--- stitchnet/stitchonnx/render.py
+++ stitchnet/stitchonnx/render.py
@@ -12,19 +12,10 @@ async def render_graph(toPath):
         "downloadPath": f"{toPath}" # TODO set your path
     })
     await page.goto('http://0.0.0.0:8081')
-    # await page.waitFor(10000)
-    await page.waitForSelector('#menu-button > svg')
-    await page.waitFor(3000)
-
-    # exportbtn = await page.querySelector('#menu-dropdown > button:nth-child(16)')
+    await page.waitForSelector('#menu-button > svg', {"visible": True})
     await page.click('#menu-button > svg')
-    await page.waitFor(1000)
+    await page.waitForSelector('#menu-dropdown > button:nth-child(16)', {"visible": True})
     await page.click('#menu-dropdown > button:nth-child(16)')
-    # element = await page.querySelector('h1')
     await page.screenshot({'path': 'example1.png'})
-    # svgtext = await svg.getProperty("outerHTML")
-    # print the article titles
-    # print(dir(svgtext.asElement()))
-    # svgvalue = await svgtext.jsonValue()
     await browser.close()
     return
